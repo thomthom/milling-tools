0997a8c674e6f41685f59e5d0facef81434c39ee
Thomas Thomassen <thomas@thomthom.net>
2017-07-29 20:51:58 +0200

    Adding replacement class for UI::Command.

--- src/tt_milling/main.rb
+++ src/tt_milling/main.rb
@@ -8,6 +8,7 @@
 
 require 'sketchup.rb'
 
+require 'tt_milling/command'
 require 'tt_milling/debug'
 require 'tt_milling/fillet'
 require 'tt_milling/ground'
@@ -18,41 +19,32 @@ require 'tt_milling/parts'
 module TT::Plugins::MillingTools
 
   unless file_loaded?( __FILE__ )
-    # TODO: Use QFT/SUbD custom command class.
-    cmd = UI::Command.new('Dog-Bone Fillets') {
+    cmd = Command.new('Dog-Bone Fillets') {
       self.dog_bone
     }
-    cmd.tooltip = 'Dog-Bone Fillets'
     cmd.status_bar_text = 'Tool to generate dog-bone fillets.'
-    cmd.large_icon = File.join(PATH, 'images', '045-drill.svg')
-    cmd.small_icon = File.join(PATH, 'images', '045-drill.svg')
+    cmd.icon = File.join(PATH, 'images', '045-drill')
     dog_bone_tool = cmd
 
-    cmd = UI::Command.new('Generate Cut Parts') {
+    cmd = Command.new('Generate Cut Parts') {
       self.generate_parts
     }
-    cmd.tooltip = 'Generate Cut Parts'
     cmd.status_bar_text = 'Generates 2D cut shapes for each part.'
-    cmd.large_icon = File.join(PATH, 'images', '006-saw-1.svg')
-    cmd.small_icon = File.join(PATH, 'images', '006-saw-1.svg')
+    cmd.icon = File.join(PATH, 'images', '006-saw-1')
     generate_parts = cmd
 
-    cmd = UI::Command.new('Adjust Part Height') {
+    cmd = Command.new('Adjust Part Height') {
       self.prompt_part_height
     }
-    cmd.tooltip = 'Adjust Part Height'
     cmd.status_bar_text = 'Adjust the height of the selected parts.'
-    cmd.large_icon = File.join(PATH, 'images', 'caliper.svg')
-    cmd.small_icon = File.join(PATH, 'images', 'caliper.svg')
+    cmd.icon = File.join(PATH, 'images', 'caliper')
     adjust_height = cmd
 
-    cmd = UI::Command.new('Set Ground Plane') {
+    cmd = Command.new('Set Ground Plane') {
       self.ground_plane_tool
     }
-    cmd.tooltip = 'Set Ground Plane'
     cmd.status_bar_text = 'Pick a face to set the ground place for instances.'
-    cmd.large_icon = File.join(PATH, 'images', 'cube-1.svg')
-    cmd.small_icon = File.join(PATH, 'images', 'cube-1.svg')
+    cmd.icon = File.join(PATH, 'images', 'cube-1')
     ground_plane_tool = cmd
 
 
---
+++ src/tt_milling/system.rb
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+#
+#    Author: Thomas Thomassen
+# Copyright: Copyright (c) 2010–2017
+#   License: None
+#
+#-------------------------------------------------------------------------------
+
+
+module TT::Plugins::MillingTools
+
+  PLATFORM_WIN = (Sketchup.platform == :platform_win)
+  PLATFORM_OSX = (Sketchup.platform == :platform_osx)
+
+end # module
